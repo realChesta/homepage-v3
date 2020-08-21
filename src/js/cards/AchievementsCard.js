07792d647ea78b3b7fc582862a4a4eff819b3ee3
Chester <chesta@kekt.us>
2020-08-21 22:03:10 +0200

    AchievementsCard: add achievements

--- src/js/cards/AchievementsCard.js
+++ src/js/cards/AchievementsCard.js
@@ -5,23 +5,58 @@ import CardWrapper from "../components/CardWrapper.js";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import IconTitle, {IconSubTitle} from "../components/IconTitle.js";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles(theme => ({
     inset: {
         margin: '0 1em'
+    },
+    aContainer: {
+        padding: theme.spacing(2),
+        marginTop: theme.spacing(1)
+    },
+    aTitleContainer: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        marginBottom: '0.5em'
+    },
+    aTitle: {
+        maxWidth: '70%',
+        flexGrow: '1',
+        lineHeight: '1em',
+    },
+    aExtra: {
+        flexGrow: '0',
+        flexShrink: '0',
+        lineHeight: '1em'
+    },
+    aDescription: {
+        lineHeight: '1em'
     }
 }));
 
 const AchievementsCard = props => {
     const classes = useStyles();
+
     return (
         <CardWrapper>
             <Card>
                 <CardContent>
-                   <IconTitle icon="star">Achievements and Awards</IconTitle>
+                    <IconTitle icon="star">Achievements and Awards</IconTitle>
                     <div className={classes.inset}>
                         <IconSubTitle icon="code">Coding Competitions</IconSubTitle>
+                        <Achievement
+                            title="Google Hash Code 2018"
+                            description="in a team of four"
+                            extra="placed 81 / 4856"
+                        />
+                        <br/>
                         <IconSubTitle icon="stars">Awards</IconSubTitle>
+                        <Achievement
+                            title="Niklaus Wirth Young Talent Computer Science Award"
+                            description="for matura paper 'SoNNic'"
+                            extra="2017"
+                        />
                     </div>
                 </CardContent>
             </Card>
@@ -29,8 +64,40 @@ const AchievementsCard = props => {
     );
 };
 
-AchievementsCard.propTypes = {
+AchievementsCard.propTypes = {};
 
-};
+const Achievement = props => {
+    const classes = useStyles();
+    return (
+        <Card variant="outlined" className={classes.aContainer}>
+                <div className={classes.aTitleContainer}>
+                    <Typography
+                        color="primary"
+                        className={classes.aTitle}
+                    >
+                        {props.title}
+                    </Typography>
+                    <Typography
+                        color="secondary"
+                        className={classes.aExtra}
+                    >
+                        {props.extra}
+                    </Typography>
+                </div>
+                <Typography
+                    color="textSecondary"
+                    className={classes.aDescription}
+                >
+                    {props.description}
+                </Typography>
+        </Card>
+    );
+}
+
+Achievement.propTypes = {
+    title: PropTypes.string,
+    description: PropTypes.string,
+    extra: PropTypes.string
+}
 
 export default AchievementsCard;
--- src/styles/Themes.js
+++ src/styles/Themes.js
@@ -24,7 +24,7 @@ export const lightTheme = createMuiTheme({
             main: '#cc667c'
         },
         secondary: {
-            main: '#fee140'
+            main: '#fa0'
         }
     },
     typography,
